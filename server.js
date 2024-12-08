0087cdbf4d0bdc8b272cd3a6e03630062271d687
Dean617 <84459600+Dean617@users.noreply.github.com>
2024-12-08 12:19:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://10202008:<db_password>@flights.huvbs.mongodb.net/?retryWrites=true&w=majority&appName=Flights", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("Error connecting to MongoDB:", err));
 
